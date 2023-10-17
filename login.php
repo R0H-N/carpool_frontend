9ec4fb884edd494bc834c3c1e8e3694c6efb5b2c
rohan <rohn.py@gmail.com>
2023-10-17 22:09:36 +0530

    some working php

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
 <?php
+echo "Reached here 1"; 
+
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Receive data from the login form
@@ -6,13 +8,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
 
     // Database connection parameters
+    echo "Reached here 1"; 
     $servername = "localhost";  // Replace with your database server address
     $username = "root";  // Replace with your database username
-    $password = "";  // Replace with your database password
-    $database = "users";  // Replace with your database name
+    $db_password = "";  // Replace with your database password
+    $database = "carpool_db";  // Replace with your database name
 
     // Create a connection to the database
-    $conn = new mysqli($servername, $username, $password, $database);
+    $conn = new mysqli($servername, $username, $db_password, $database);
 
     // Check the connection
     if ($conn->connect_error) {
@@ -22,22 +25,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Query the database to check if the user exists
     $sql = "SELECT * FROM users WHERE email = '$email'";
     $result = $conn->query($sql);
+    echo "Reached here 2"; 
 
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
+        echo "Reached here 3"; 
         $stored_password = $row['password'];
+        echo "Reached here 4 $stored_password"; 
+        echo "$password";
         // Verify the entered password against the stored hashed password
         if (password_verify($password, $stored_password)) {
             // Authentication successful
-            session_start(); // Start a session (if not already started)
+           
+            echo "Reached here 5"; 
+           
+            echo "Reached here 1"; 
+
             $_SESSION['email'] = $email; // Store user's email in a session variable
             // Redirect the user to the dashboard or home page
-            header("Location: dashboard.php"); // Change the URL to your dashboard page
+            header("Location: loginpage.html"); // Change the URL to your dashboard page
             exit();
         } else {
             // Authentication failed (incorrect password)
             $error_message = "Incorrect email or password.";
-        }
+            echo "$error_message";
+            
+            }
+
     } else {
         // Authentication failed (user not found)
         $error_message = "User not found.";
@@ -46,6 +60,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Close the database connection
     $conn->close();
 }
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // If not submitted or authentication failed, display the login form
 ?>
